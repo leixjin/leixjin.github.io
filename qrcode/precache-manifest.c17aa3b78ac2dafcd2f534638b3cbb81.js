b178aa07e745f5ef25297ac8223ac836386ee0fc
金晓磊 <xiaolei.jin@okg.com>
2022-05-23 11:08:32 +0800

    add qrcode

---
+++ qrcode/precache-manifest.c17aa3b78ac2dafcd2f534638b3cbb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbef1b372c08c6d34c15461721f8385a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81b86d88945efec7837e",
+    "url": "./static/css/main.07c21c04.chunk.css"
+  },
+  {
+    "revision": "cfa52b38af7be2c584d1",
+    "url": "./static/js/2.f192477f.chunk.js"
+  },
+  {
+    "revision": "c2dc3a0da7ac76b30a247ebb412c0352",
+    "url": "./static/js/2.f192477f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b86d88945efec7837e",
+    "url": "./static/js/main.24900e98.chunk.js"
+  },
+  {
+    "revision": "3ad1574b2c6b0f41b86d",
+    "url": "./static/js/runtime-main.5bdb13fd.js"
+  },
+  {
+    "revision": "5e638c9a6be0d68e9a635ad74cafdb22",
+    "url": "./static/media/Futura.5e638c9a.ttc"
+  }
+]);
